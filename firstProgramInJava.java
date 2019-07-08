3509b28953b6bb46b43ca44fbece98bacb6d0285
DMoreno06 <mordiana1506@gmail.com>
2019-07-08 12:07:35 -0500

    firs program in java

---
+++ firstProgramInJava.java
@@ -0,0 +1,10 @@
+/**
+ * firstProgramInJava
+ */
+public class firstProgramInJava {
+
+    public static void main(String[] args) {
+        System.out.println("¡ hola para todos :) !");
+    }
+
+}
